9dad9df6594c3151ebd51ae6a971af9244ed42a6
MattiaDif <di.florio.mattia@gmail.com>
2022-01-22 10:45:51 +0100

    minor

--- Spike_Detection_models/SingleChannelModels/Energy_Operator/float_sch_run_SWTTEO.m
+++ Spike_Detection_models/SingleChannelModels/Energy_Operator/float_sch_run_SWTTEO.m
@@ -15,11 +15,12 @@ end
 
 
 mdl_name = "float_sch_SWTTEO";
+noise_level = {'20' '30'};
 
 
 %wavelet
 wname = 'sym5';
-iter = 2;
+% iter = 2;
 %[phi,psi,xval] = wavefun(wname,iter);
 wav_approx = [-0.019538882735286728
                 -0.021101834024758855
@@ -52,13 +53,13 @@ fs = 30000; %Hz - sampling frequency
 fn = fs/2;  %Hz - Nyquist frequency
 refractory = 10^-3; %refractory period
 TEO_buffer = length(wav_approx)-1;    %TEO buffer length
-TEO_buffer_overlap = 0;    %TEO buffer overlap
+TEO_buffer_overlap = TEO_buffer-1;    %TEO buffer overlap
 feature_buffer = fs;    %feature buffer length
-feature_gain = [3];   %adaptive threshold gain
+feature_gain = [2:0.1:7];   %adaptive threshold gain
 hamming_w_length = 32;  %hamming window length in samples
 buffer_hamming_length = hamming_w_length;   %buffer length before smoothing in samples
 buffer_hamming_length_overlap = buffer_hamming_length - 1;
-sim_type = 'normal'; %simulation speed
+sim_type = 'rapid'; %simulation speed
 sim_stop_time = '10';   %s
 
 
@@ -70,133 +71,141 @@ peak_diff = 25; %samples --> max spike position distance between recording and g
 spiketrain = 3; %ground_truth selected for performance evaluation
 %peak_diff --> tolerance
 
-%% Data loading
-filename = 'ch15_neuronexus32_recording_10';
-
-signal = load([filename,'.mat']);
-ground = load([filename,'_gt.mat']);
-
-% load(['sim_results_',num2str(noise_level),'.mat']);
-
-
-%% Simulation with different thresholds
-numSims = length(feature_gain);   %number of simulation depending on number of feature gain
-
-%Simulation parameters
-mdl=convertStringsToChars(mdl_name);
-load_system(mdl);
-set_param(mdl, 'SimulationMode', sim_type)
-set_param(mdl,'StartTime','0','StopTime',sim_stop_time)
-BlockPaths = find_system(mdl,'Type','Block')
-BlockDialogParameters = get_param([mdl '/th gain'],'DialogParameters')
-
-%Input setting
-for curr_sim = 1:numSims
-    in(curr_sim) = Simulink.SimulationInput(mdl);
-    in(curr_sim) = setBlockParameter(in(curr_sim), [mdl '/th gain'], 'Gain', num2str(feature_gain(curr_sim)));
-end
-
-%Simulation running
-out = sim(in,'ShowProgress', 'on');
-
-
-%% Get simulation output
-for curr_sim = 1:numSims
+for i = 1:length(noise_level)
     
-    simOut = out(curr_sim);
-    ground_truth_ts(curr_sim,:) = simOut.logsout.get('ground_truth').Values;
-    recording_ts(curr_sim,:) = simOut.logsout.get('recording').Values;
-    SWTTEO_ts(curr_sim,:) = simOut.logsout.get('SWTTEO').Values;
-    threshold_ts(curr_sim,:) = simOut.logsout.get('threshold').Values;
-    SWTTEO_above_th_ts(curr_sim,:) = simOut.logsout.get('SWTTEO_above_th').Values;
-    spikes_ts(curr_sim,:) = simOut.logsout.get('spikes').Values;
-    interspike_ts(curr_sim,:) = simOut.logsout.get('interspike').Values;
+    FPrate = [];
+    TPrate = [];
 
-    ground_truth(curr_sim,:) = ground_truth_ts(curr_sim).Data(:,spiketrain);
-    recording(curr_sim,:) = recording_ts(curr_sim).Data;
-    SWTTEO(curr_sim,:) = SWTTEO_ts(curr_sim).Data;
-    threshold(curr_sim,:) = threshold_ts(curr_sim).Data;
-    SWTTEO_above_th(curr_sim,:) = SWTTEO_above_th_ts(curr_sim).Data;
-    spikes(curr_sim,:) = spikes_ts(curr_sim).Data;
-    interspike(curr_sim,:) = interspike_ts(curr_sim).Data;
+    %% Data loading
+    filename = ['ch15_neuronexus32_recording_',char(noise_level(i))]
+
+    signal = load([filename,'.mat']);
+    ground = load([filename,'_gt.mat']);
+
+    signal = load([filename,'.mat']);
+    ground = load([filename,'_gt.mat']);
+
+    % load(['sim_results_',num2str(noise_level),'.mat']);
 
 
+    %% Simulation with different thresholds
+    numSims = length(feature_gain);   %number of simulation depending on number of feature gain
 
-    % Performance evaluation
-    P(curr_sim) = sum(round(ground_truth(curr_sim,:)));    %P    %round due to some quantization error (some samples were e-11 instead of 0)
-    NDS(curr_sim) = sum(round(spikes(curr_sim,:)));  %NDS
+    %Simulation parameters
+    mdl=convertStringsToChars(mdl_name);
+    load_system(mdl);
+    set_param(mdl, 'SimulationMode', sim_type)
+    set_param(mdl,'StartTime','0','StopTime',sim_stop_time)
+    BlockPaths = find_system(mdl,'Type','Block')
+    BlockDialogParameters = get_param([mdl '/th gain'],'DialogParameters')
 
-    spikes_locks{curr_sim,:} = find(round(spikes(curr_sim,:)));    %samples
-    ground_locks{curr_sim,:} = find(round(ground_truth(curr_sim,:))); %samples
-    
-    TP(curr_sim) = 0;
-    for i=1:length(spikes_locks{curr_sim,:})
-        locks_diff = [];
-        TP_temp = [];
-        locks_diff = abs(spikes_locks{curr_sim,:}(i) - ground_locks{curr_sim,:});
-        TP_temp = find(locks_diff <= peak_diff);
-        if isempty(TP_temp)
-            TP(curr_sim) = TP(curr_sim);
-        else
-            TP(curr_sim) = TP(curr_sim) + 1;
-        end
+    %Input setting
+    for curr_sim = 1:numSims
+        in(curr_sim) = Simulink.SimulationInput(mdl);
+        in(curr_sim) = setBlockParameter(in(curr_sim), [mdl '/th gain'], 'Gain', num2str(feature_gain(curr_sim)));
     end
 
-    FN(curr_sim) = P(curr_sim) - TP(curr_sim);
-    FP(curr_sim) = NDS(curr_sim) - TP(curr_sim);
-    
-    N(curr_sim) = ((length(recording(curr_sim,:)))-P(curr_sim)*w_len)/w_len;
-    
-    TN(curr_sim) = N(curr_sim) - FP(curr_sim);
-    accuracy(curr_sim) = (TP(curr_sim) + TN(curr_sim))/(P(curr_sim)+N(curr_sim));
-    perf(curr_sim) = TP(curr_sim)/(FP(curr_sim)+FN(curr_sim));
-    eff(curr_sim) = perf(curr_sim)/(perf(curr_sim)+1);
-    sens(curr_sim) = TP(curr_sim)/(TP(curr_sim)+FN(curr_sim));
-    spec(curr_sim) = TN(curr_sim)/N(curr_sim);
-    prec(curr_sim) = TP(curr_sim)/(TP(curr_sim)+FP(curr_sim));
-    NPV(curr_sim) = TN(curr_sim)/(TN(curr_sim)+FN(curr_sim));
-    FNR(curr_sim) = FN(curr_sim)/(FN(curr_sim)+TP(curr_sim));
-    FPR(curr_sim) = FP(curr_sim)/(FP(curr_sim)+TP(curr_sim));
-    F1score(curr_sim) = 2*TP(curr_sim)/(2*TP(curr_sim)+FN(curr_sim)+FP(curr_sim));
-    MCC(curr_sim) = (TP(curr_sim)*TN(curr_sim)-FP(curr_sim)*FN(curr_sim))/...
-                    sqrt((TP(curr_sim)+FP(curr_sim))*(TP(curr_sim)+FN(curr_sim))*(TN(curr_sim)+FP(curr_sim))*(TN(curr_sim)+FN(curr_sim)));
+    %Simulation running
+    out = sim(in,'ShowProgress', 'on');
 
-    FPrate(curr_sim) = FP(curr_sim)/N(curr_sim);
-    TPrate(curr_sim) = TP(curr_sim)/P(curr_sim);
-        
+
+    %% Get simulation output
+    for curr_sim = 1:numSims
+
+        simOut = out(curr_sim);
+        ground_truth_ts(curr_sim,:) = simOut.logsout.get('ground_truth').Values;
+        recording_ts(curr_sim,:) = simOut.logsout.get('recording').Values;
+        SWTTEO_ts(curr_sim,:) = simOut.logsout.get('SWTTEO').Values;
+        threshold_ts(curr_sim,:) = simOut.logsout.get('threshold').Values;
+        SWTTEO_above_th_ts(curr_sim,:) = simOut.logsout.get('SWTTEO_above_th').Values;
+        spikes_ts(curr_sim,:) = simOut.logsout.get('spikes').Values;
+        interspike_ts(curr_sim,:) = simOut.logsout.get('interspike').Values;
+
+        ground_truth(curr_sim,:) = ground_truth_ts(curr_sim).Data(:,spiketrain);
+        recording(curr_sim,:) = recording_ts(curr_sim).Data;
+        SWTTEO(curr_sim,:) = SWTTEO_ts(curr_sim).Data;
+        threshold(curr_sim,:) = threshold_ts(curr_sim).Data;
+        SWTTEO_above_th(curr_sim,:) = SWTTEO_above_th_ts(curr_sim).Data;
+        spikes(curr_sim,:) = spikes_ts(curr_sim).Data;
+        interspike(curr_sim,:) = interspike_ts(curr_sim).Data;
+
+
+
+        % Performance evaluation
+        P(curr_sim) = sum(round(ground_truth(curr_sim,:)));    %P    %round due to some quantization error (some samples were e-11 instead of 0)
+        NDS(curr_sim) = sum(round(spikes(curr_sim,:)));  %NDS
+
+        spikes_locks{curr_sim,:} = find(round(spikes(curr_sim,:)));    %samples
+        ground_locks{curr_sim,:} = find(round(ground_truth(curr_sim,:))); %samples
+
+        TP(curr_sim) = 0;
+        for i=1:length(spikes_locks{curr_sim,:})
+            locks_diff = [];
+            TP_temp = [];
+            locks_diff = abs(spikes_locks{curr_sim,:}(i) - ground_locks{curr_sim,:});
+            TP_temp = find(locks_diff <= peak_diff);
+            if isempty(TP_temp)
+                TP(curr_sim) = TP(curr_sim);
+            else
+                TP(curr_sim) = TP(curr_sim) + 1;
+            end
+        end
+
+        FN(curr_sim) = P(curr_sim) - TP(curr_sim);
+        FP(curr_sim) = NDS(curr_sim) - TP(curr_sim);
+
+        N(curr_sim) = ((length(recording(curr_sim,:)))-P(curr_sim)*w_len)/w_len;
+
+        TN(curr_sim) = N(curr_sim) - FP(curr_sim);
+        accuracy(curr_sim) = (TP(curr_sim) + TN(curr_sim))/(P(curr_sim)+N(curr_sim));
+        perf(curr_sim) = TP(curr_sim)/(FP(curr_sim)+FN(curr_sim));
+        eff(curr_sim) = perf(curr_sim)/(perf(curr_sim)+1);
+        sens(curr_sim) = TP(curr_sim)/(TP(curr_sim)+FN(curr_sim));
+        spec(curr_sim) = TN(curr_sim)/N(curr_sim);
+        prec(curr_sim) = TP(curr_sim)/(TP(curr_sim)+FP(curr_sim));
+        NPV(curr_sim) = TN(curr_sim)/(TN(curr_sim)+FN(curr_sim));
+        FNR(curr_sim) = FN(curr_sim)/(FN(curr_sim)+TP(curr_sim));
+        FPR(curr_sim) = FP(curr_sim)/(FP(curr_sim)+TP(curr_sim));
+        F1score(curr_sim) = 2*TP(curr_sim)/(2*TP(curr_sim)+FN(curr_sim)+FP(curr_sim));
+        MCC(curr_sim) = (TP(curr_sim)*TN(curr_sim)-FP(curr_sim)*FN(curr_sim))/...
+                        sqrt((TP(curr_sim)+FP(curr_sim))*(TP(curr_sim)+FN(curr_sim))*(TN(curr_sim)+FP(curr_sim))*(TN(curr_sim)+FN(curr_sim)));
+
+        FPrate(curr_sim) = FP(curr_sim)/N(curr_sim);
+        TPrate(curr_sim) = TP(curr_sim)/P(curr_sim);
+
+    end
+
+
+    %% ROC, AUC
+    FPrate = [1 FPrate 0];
+    TPrate = [1 TPrate 0];
+
+    figure
+    plot(flip(FPrate),flip(TPrate),'r','LineWidth',2)
+    xlabel('FP rate')
+    ylabel('TP rate')
+    title('Hard Threshold ROC')
+    set(gca,'FontSize',14)
+    axis([0 1 0 1])
+
+    AUC = -trapz(FPrate,TPrate);
+
+
+
+
+    %% Saving resuls
+
+    type = strcat([mdl, '_', filename(end-1:end)]);
+
+    result.(type).AUC = AUC;
+    result.(type).FPrate = FPrate;
+    result.(type).TPrate = TPrate;
+    result.(type).threshold = feature_gain;
+    result.(type).noise_level = str2double(filename(end-1:end));
+
+    save('D:\GitHub\model-based-spike-detection\Spike_Detection_models\Results\result.mat','result');
+
 end
-
-
-%% ROC, AUC
-FPrate = [1 FPrate 0];
-TPrate = [1 TPrate 0];
-
-figure
-plot(flip(FPrate),flip(TPrate),'r','LineWidth',2)
-xlabel('FP rate')
-ylabel('TP rate')
-title('Hard Threshold ROC')
-set(gca,'FontSize',14)
-axis([0 1 0 1])
-
-AUC = -trapz(FPrate,TPrate);
-
-
-
-
-%% Saving resuls
-
-type = strcat([mdl, '_', filename(end-1:end)]);
-
-result.(type).AUC = AUC;
-result.(type).FPrate = FPrate;
-result.(type).TPrate = TPrate;
-result.(type).threshold = feature_gain;
-result.(type).noise_level = str2double(filename(end-1:end));
-
-save('D:\GitHub\model-based-spike-detection\Spike_Detection_models\Results\result.mat','result');
-
-
 
 
 
